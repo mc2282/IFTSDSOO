728dfec2003113ddacd753453445f1d7319b34c4
m2028 <145708751+m2028@users.noreply.github.com>
2023-09-25 15:45:14 -0300

    Add files via upload

---
+++ S02PF1/SuperHeroes/Program.cs
@@ -0,0 +1,43 @@
+﻿using SuperHeroes;
+// See https://aka.ms/new-console-template for more information
+
+
+/* En una galaxia lejana habitan muchos superhéroes con habilidades extraordinarias, estos
+han decidido realizar una competencia entre ellos comparando sus distintos atributos. Para
+ello, necesitamos crear la clase "SuperHeroe" que nos permita representar a estos
+poderosos personajes con los atributos nombre (String), fuerza (int), resistencia (int) y
+superpoderes (int). 
+Todos los atributos numéricos deberán aceptar valores entre 0 y 100; en caso que el setter
+correspondiente reciba un valor fuera de rango deberá setear el valor límite  correspondiente
+(si recibe un valor negativo asignar cero, si recibe uno superior a cien, asignar cien).
+El constructor de la clase recibirá todos los valores de sus atributos por parámetro y 
+usará los setters (todos privados) para validar el rango correcto de los poderes del superhéroe.
+Se deberá crear un método competir() el cual recibirá otro superhéroe como parámetro y ,
+comparando los  poderes de él mismo contra el otro recibido por  parámetro, devolverá
+TRIUNFO , EMPATE o  DERROTA, dependiendo del resultado. 
+Para triunfar un superhéroe debe superar al otro en al menos 2 de los 3 ítems.
+******************************************************************************
+¿Qué te proponemos realizar?
+Escribir la clase Test que contenga el método main() para probar el correcto funcionamiento de la clase previamente
+realizada con el siguiente ejemplo:
+superHeroe1: Nombre: “Batman”, Fuerza: 90, Resistencia: 70, Superpoderes: 0
+superHeroe2: Nombre: “Superman”, Fuerza: 95, Resistencia: 60, Superpoderes: 70
+1. Hacer jugar al superheroe1 pasándole el objeto superheroe2 y mostrar el resultado por pantalla. 
+2. Chequear el resultado (debería ser DERROTA).
+3. Hacer jugar al superheroe2 contra el superheroe1 y mostrar el resultado por pantalla. 
+4. Chequear el resultado (debería ser TRIUNFO).
+5. Crear más superhéroes con distintos valores. */
+
+namespace S02PF1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            SuperHeroe superheroe1 = new SuperHeroe("Batman", 90, 70, 0);
+            SuperHeroe superheroe2 = new SuperHeroe("Superman", 95, 60, 70);
+
+            superheroe1.competir(superheroe2);
+        }
+    }
+}
---
+++ S02PF1/SuperHeroes/Superheroe.cs
@@ -0,0 +1,129 @@
+﻿namespace SuperHeroes
+{
+    internal class SuperHeroe
+    {
+        private string nombre;
+        private int fuerza;
+        private int resistencia;
+        private int superpoderes;
+
+        public SuperHeroe(string nombre, int fuerza, int resistencia, int superpoderes)
+        {
+            this.Nombre = nombre;
+            this.Fuerza = fuerza;
+            this.Resistencia = resistencia;
+            this.Superpoderes = superpoderes;
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Fuerza
+        {
+            get { return fuerza; }
+            set
+            {
+                // Aplicar validación de límites
+                if (value < 0)
+                {
+                    fuerza = 0;
+                }
+                else if (value > 100)
+                {
+                    fuerza = 100;
+                }
+                else
+                {
+                    fuerza = value;
+                }
+            }
+        }
+        public int Resistencia
+        {
+            get { return resistencia; }
+            set
+            {
+                // Aplicar validación de límites
+                if (value < 0)
+                {
+                    resistencia = 0;
+                }
+                else if (value > 100)
+                {
+                    resistencia = 100;
+                }
+                else
+                {
+                    resistencia = value;
+                }
+            }
+        }
+        public int Superpoderes
+        {
+            get { return superpoderes; }
+            set
+            {
+                // Aplicar validación de límites
+                if (value < 0)
+                {
+                    superpoderes = 0;
+                }
+                else if (value > 100)
+                {
+                    superpoderes = 100;
+                }
+                else
+                {
+                    superpoderes = value;
+                }
+            }
+        }
+        public void competir(SuperHeroe otroSuperHeroe)
+        {
+            int puntajeSuperHeroe1 = 0;
+            int puntajeSuperHeroe2 = 0;
+
+            if (Fuerza > otroSuperHeroe.Fuerza)
+            {
+                puntajeSuperHeroe1++;
+            }
+            else
+            {
+                puntajeSuperHeroe2++;
+            }
+
+            if (Resistencia > otroSuperHeroe.Resistencia)
+            {
+                puntajeSuperHeroe1++;
+            }
+            else
+            {
+                puntajeSuperHeroe2++;
+            }
+
+            if (Superpoderes > otroSuperHeroe.Superpoderes)
+            {
+                puntajeSuperHeroe1++;
+            }
+            else
+            {
+                puntajeSuperHeroe2++;
+            }
+
+            if (puntajeSuperHeroe1 > puntajeSuperHeroe2)
+            {
+                Console.WriteLine("TRIUNFO");
+            }
+            else if (puntajeSuperHeroe1 == puntajeSuperHeroe2)
+            {
+                Console.WriteLine("EMPATE");
+            }
+            else
+            {
+                Console.WriteLine("DERROTA");
+            }
+        }
+    }
+}
